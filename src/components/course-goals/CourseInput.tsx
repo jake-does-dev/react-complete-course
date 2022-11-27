a08eb095d5675e6e2c02a0e274b9eace31d3ad9e
Jake Allan <jjemail1995@gmail.com>
2022-11-27 11:31:41 +0000

    CSS modules

--- src/components/course-goals/CourseInput.tsx
+++ src/components/course-goals/CourseInput.tsx
@@ -1,51 +1,11 @@
 import React, { ChangeEvent, FormEvent, useState } from 'react';
 
 import Button from '../ui/Button';
-import styled from 'styled-components';
+import styles from './CourseInput.module.css';
 
 interface CourseInputProps {
   onAddGoal(enteredValue: string): void;
 }
-
-interface FormProps {
-  readonly isValid: boolean;
-}
-
-const FormControl = styled.div<FormProps>`
-  margin: 0.5rem 0;
-
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${({ isValid }) => (isValid ? 'black' : 'red')};
-  }
-
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${({ isValid }) => (isValid ? '#ccc' : 'red')};
-    background: 1px solid ${({ isValid }) => (isValid ? 'transparent' : '#ffd7d7')};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
-
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
-
-  &.invalid input {
-    border-color: red;
-    background: #ffd7d7;
-  }
-
-  &.invalid label {
-    border-color: ${(props) => (props.isValid ? 'black' : 'red')};
-  }
-`;
 
 const CourseInput = ({ onAddGoal }: CourseInputProps) => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -69,10 +29,10 @@ const CourseInput = ({ onAddGoal }: CourseInputProps) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl isValid={isValid}>
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
         <label>Course Goal</label>
         <input type='text' onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type='submit'>Add Goal</Button>
     </form>
   );
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,28 +1,17 @@
-import styled from 'styled-components';
+import React from 'react';
+import styles from './Button.module.css';
 
-// This really is just a button; any props on `button` get passed to our styled.button.
-// Subsequently, they're used in the same way as a standard `button`!
-const Button = styled.button`
-  & {
-    font: inherit;
-    padding: 0.5rem 1.5rem;
-    border: 1px solid #8b005d;
-    color: white;
-    background: #8b005d;
-    box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-    cursor: pointer;
-  }
+interface ButtonProps {
+  type: 'button' | 'reset' | 'submit' | undefined;
+  children: string;
+}
 
-  &:focus {
-    outline: none;
-  }
-
-  &:hover,
-  &:active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
-`;
+const Button = ({ children, type }: ButtonProps) => {
+  return (
+    <button type={type} className={styles.button}>
+      {children}
+    </button>
+  );
+};
 
 export default Button;
